affde2a958ae14793fe09fe871e6b4281a588401
root27 <oguzhaneee@gmail.com>
2025-03-18 14:56:26 +0300

    structs modified

--- src/paytr/structs/structs.rs
+++ src/paytr/structs/structs.rs
@@ -8,28 +8,28 @@ pub struct Payment {
     pub user_ip: String,
     pub merchant_oid: String,
     pub email: String,
-    pub total_amount: i64,
+    pub payment_amount: i64,
     pub currency: String,
-    pub basket: String,
+    pub user_basket: String,
     pub no_installment: i64,
     pub max_installment: i64,
     pub paytr_token: String,
-    pub username: String,
+    pub user_name: String,
     pub user_address: String,
     pub user_phone: String,
-    pub ok_url: String,
-    pub fail_url: String,
+    pub merchant_ok_url: String,
+    pub merchant_fail_url: String,
     pub test_mode: String,
     pub debug_on: i8,
-    pub timeout: i64,
+    pub timeout_limit: i64,
     pub lang: String,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct PaytrResponse {
-    pub status: i16,
-    pub token: String,
-    pub reason: String,
+    pub status: Option<String>,
+    pub token: Option<String>,
+    pub reason: Option<String>,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
